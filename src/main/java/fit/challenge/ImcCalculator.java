5493bc7385078ef6fd565a5f44f57755339192db
Andrê Reges <dreeh.silva@hotmail.com>
2021-04-12 20:48:53 -0300

    Implemented class ImcCalculator and method calculate

---
+++ src/main/java/fit/challenge/ImcCalculator.java
@@ -0,0 +1,23 @@
+package fit.challenge;
+
+public class ImcCalculator {
+
+	/**
+	 * Calculate IMC
+	 * @param weight
+	 * @param height
+	 * @return String
+	 */
+	public static String calculate(double weight, double height) {
+		
+		double imc = weight / Math.pow(height, 2);
+
+		if (imc < 18.5) return "Magreza";		
+		if (imc >= 18 && imc < 24.9) return "Normal";		
+		if (imc >= 24.9 && imc < 30) return "Sobrepeso";		
+		if (imc > 30) return "Obesidade";
+		
+		return "Erro inesperado!";
+	}
+
+}
